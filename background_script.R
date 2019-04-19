c4042e9257fc815c46e6b95219e38d9aef5b91fe
Beau Meche <beau_meche@college.harvard.edu>
2019-04-19 15:15:35 -0400

    initial commmit with relevant data in the shiny folder

---
+++ background_script.R
@@ -0,0 +1,11 @@
+download.file("https://github.com/BeauMeche/Final_proj_milestone_3/raw/master/Census_2017_edu_attainment.csv",
+              destfile = "2017_data.csv",
+              mode = "wb")
+
+download.file("https://github.com/BeauMeche/Final_proj_milestone_3/raw/master/Census_edu_attainment_2016.csv",
+              destfile = "2016_data.csv", 
+              mode = "wb")
+
+download.file("https://github.com/BeauMeche/Final_proj_milestone_3/raw/master/Census_edu_attainment_2015.csv",
+              destfile = "2015_data.csv",
+              mode = "wb")
