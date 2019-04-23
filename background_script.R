17c8bc926af414f68b866c38bfcbfc6384fc10a0
Beau Meche <beau_meche@college.harvard.edu>
2019-04-23 19:31:55 -0400

    half tidy 2017 data

--- background_script.R
+++ background_script.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(stringr)
 library(janitor)
-
+library(gt)
 download.file("https://github.com/BeauMeche/Final_proj_milestone_3/raw/master/Census_2017_edu_attainment.csv",
               destfile = "2017_data.csv",
               mode = "wb")
@@ -15,44 +15,60 @@ download.file("https://github.com/BeauMeche/Final_proj_milestone_3/raw/master/Ce
               mode = "wb")
 
 data_2017 <- read_csv(file = "2017_data.csv", skip = 1) %>% 
-  clean_names() %>% 
-  census_2017_nomargin <- data_2017[, ! str_detect(names(data_2017), pattern = "margin_of_error")] 
+  clean_names()
+
+census_2017_nomargin <- data_2017[, ! str_detect(names(data_2017), pattern = "margin_of_error")] 
 
 
-
-
-
-# ORIGINAL DATA
+# ORIGINAL DATA_______________________
 
 data_2016 <- read_csv(file = "2016_data.csv", skip = 1)
-census_2016_nomargin <- data_2016[, ! str_detect(names(data_2016), pattern = "Margin of Error")]
+census_2016_nomargin <- data_2016[, ! str_detect(names(data_2016), pattern = "margin_of_error")]
 
 data_2015 <- read_csv(file = "2015_data.csv", skip = 1)
-census_2015_nomargin <- data_2015[, ! str_detect(names(data_2015), pattern = "Margin of Error")]
+census_2015_nomargin <- data_2015[, ! str_detect(names(data_2015), pattern = "margin_of_error")]
 
-# CREATE GENDER OPTIONS
+# CREATE GENDER OPTIONS_________________________
 
-males_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "Male")|
-                                     str_detect(names(census_2017_nomargin), pattern = "Geography")] %>% 
+males_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "male")|
+                                     str_detect(names(census_2017_nomargin), pattern = "geography")] %>% 
   mutate(gender = "male")
 
-females_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "Female")|
-                                     str_detect(names(census_2017_nomargin), pattern = "Geography")] %>% 
+females_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "female")|
+                                     str_detect(names(census_2017_nomargin), pattern = "geography")] %>% 
   mutate(gender = "female")
 
 # CREATE TOTALS
 
-total_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "Total")|
-                                     str_detect(names(census_2017_nomargin), pattern = "Geography")] %>% 
+total_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "total")|
+                                     str_detect(names(census_2017_nomargin), pattern = "geography")] %>% 
   mutate(gender = "NA")
 
-# GET RID OF NAME EXTRAS ETC. 
+# GET RID OF UNNEEDED NAME COMPONENTS
 
 names(total_2017) <- total_2017 %>% 
   names() %>% 
   gsub(x = colnames(total_2017),
-       pattern = "Total; Estimate;",
+       pattern = "total_estimate_",
        replacement = "")
+
+names(males_2017) <- males_2017 %>% 
+  names() %>% 
+  gsub(x = colnames(males_2017),
+       pattern = "male_estimate_",
+       replacement = "")
+
+names(females_2017) <- females_2017 %>% 
+  names() %>% 
+  gsub(x = colnames(females_2017),
+       pattern = "female_estimate_",
+       replacement = "")
+
+nice_2017 <- census_2017_nomargin %>% 
+  gather(total_estimate_population_18_to_24_years:percent_female_estimate_median_earnings_in_the_past_12_months_in_2017_inflation_adjusted_dollars_population_25_years_and_over_with_earnings_graduate_or_professional_degree,
+         key = "key",
+         value = "value", 
+         na.rm = TRUE)
 
 
 # old plot, not quite relevant
@@ -62,6 +78,7 @@ names(total_2017) <- total_2017 %>%
 # ggplot() +
 #   geom_jitter(aes(x = "Total; Estimate; Population 18 to 24 years",
 #                  y = "Geography")) + 
+
 #   theme_minimal() + 
 #   labs(x = "States", y = "Estimated Population of Men")
 
