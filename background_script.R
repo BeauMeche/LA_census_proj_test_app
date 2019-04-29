9252f90d9117bdfcd6d4543c5cc2f0b834b5a475
Beau Meche <beau_meche@college.harvard.edu>
2019-04-29 13:42:39 -0400

    made 1/3 of my complete data tables

--- background_script.R
+++ background_script.R
@@ -25,10 +25,14 @@ census_2017_nomargin <- data_2017[, ! str_detect(names(data_2017), pattern = "ma
 
 # ORIGINAL DATA_______________________
 
-data_2016 <- read_csv(file = "2016_data.csv", skip = 1)
+data_2016 <- read_csv(file = "2016_data.csv", skip = 1) %>% 
+  clean_names()
+
 census_2016_nomargin <- data_2016[, ! str_detect(names(data_2016), pattern = "margin_of_error")]
 
-data_2015 <- read_csv(file = "2015_data.csv", skip = 1)
+data_2015 <- read_csv(file = "2015_data.csv", skip = 1) %>% 
+  clean_names() 
+
 census_2015_nomargin <- data_2015[, ! str_detect(names(data_2015), pattern = "margin_of_error")]
 
 # CREATE GENDER OPTIONS_________________________
@@ -37,41 +41,41 @@ males_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pat
                                      str_detect(names(census_2017_nomargin), pattern = "geography")] %>% 
   mutate(gender = "male")
 
-females_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "female")|
-                                     str_detect(names(census_2017_nomargin), pattern = "geography")] %>% 
-  mutate(gender = "female")
+# females_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "female")|
+#                                      str_detect(names(census_2017_nomargin), pattern = "geography")] %>% 
+#   mutate(gender = "female")
+# 
+# # CREATE TOTALS
+# 
+# total_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "total")|
+#                                      str_detect(names(census_2017_nomargin), pattern = "geography")] %>% 
+#   mutate(gender = "NA")
 
-# CREATE TOTALS
-
-total_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "total")|
-                                     str_detect(names(census_2017_nomargin), pattern = "geography")] %>% 
-  mutate(gender = "NA")
-
-# GET RID OF UNNEEDED NAME COMPONENTS
-
-names(total_2017) <- total_2017 %>% 
-  names() %>% 
-  gsub(x = colnames(total_2017),
-       pattern = "total_estimate_",
-       replacement = "")
-
-names(males_2017) <- males_2017 %>% 
-  names() %>% 
-  gsub(x = colnames(males_2017),
-       pattern = "male_estimate_",
-       replacement = "")
-
-names(females_2017) <- females_2017 %>% 
-  names() %>% 
-  gsub(x = colnames(females_2017),
-       pattern = "female_estimate_",
-       replacement = "")
-
-nice_2017 <- census_2017_nomargin %>% 
-  gather(total_estimate_population_18_to_24_years:percent_female_estimate_median_earnings_in_the_past_12_months_in_2017_inflation_adjusted_dollars_population_25_years_and_over_with_earnings_graduate_or_professional_degree,
-         key = "key",
-         value = "value", 
-         na.rm = TRUE)
+# # GET RID OF UNNEEDED NAME COMPONENTS
+# 
+# names(total_2017) <- total_2017 %>% 
+#   names() %>% 
+#   gsub(x = colnames(total_2017),
+#        pattern = "total_estimate_",
+#        replacement = "")
+# 
+# names(males_2017) <- males_2017 %>% 
+#   names() %>% 
+#   gsub(x = colnames(males_2017),
+#        pattern = "male_estimate_",
+#        replacement = "")
+# 
+# names(females_2017) <- females_2017 %>% 
+#   names() %>% 
+#   gsub(x = colnames(females_2017),
+#        pattern = "female_estimate_",
+#        replacement = "")
+# 
+# nice_2017 <- census_2017_nomargin %>% 
+#   gather(total_estimate_population_18_to_24_years:percent_female_estimate_median_earnings_in_the_past_12_months_in_2017_inflation_adjusted_dollars_population_25_years_and_over_with_earnings_graduate_or_professional_degree,
+#          key = "key",
+#          value = "value", 
+#          na.rm = TRUE)
 
             # This code is wrong, but the processs works so I am keeping it for reference
             # for now.
@@ -120,11 +124,11 @@ ggplot(pops_of_2017) +
 
 
 
-test <- nice_2017 %>% 
-  if (str_detect(key, "18_to")) {
-    separate(key, pattern = str_extract(key, age)) } else{
-      str_replace(key, "__")
-    }
+# test <- nice_2017 %>% 
+#   if (str_detect(key, "18_to")) {
+#     separate(key, pattern = str_extract(key, age)) } else{
+#       str_replace(key, "__")
+#     }
       
 
 
--- script_2.R
+++ script_2.R
@@ -6,7 +6,7 @@ library(dplyr)
 library(ggthemes)
 
 
-
+# dealing with total population change here
 
 total_pop_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin), pattern = "total_estimate_population")|
                                        str_detect(names(census_2017_nomargin), pattern = "geography")] %>%
@@ -14,3 +14,19 @@ total_pop_2017 <- census_2017_nomargin[, str_detect(names(census_2017_nomargin),
            total_estimate_population_35_to_44_years + total_estimate_population_45_to_64_years +
            total_estimate_population_65_years_and_over) %>%
   select(geography, totalpop)
+
+
+total_pop_2016 <- census_2016_nomargin[, str_detect(names(census_2016_nomargin), pattern = "total_estimate_population")|
+                                         str_detect(names(census_2016_nomargin), pattern = "geography")] %>%
+  mutate(totalpop = total_estimate_population_18_to_24_years + total_estimate_population_25_to_34_years +
+           total_estimate_population_35_to_44_years + total_estimate_population_45_to_64_years +
+           total_estimate_population_65_years_and_over) %>%
+  select(geography, totalpop)
+
+# this data shows population flux by state from 2016-2017. Percentage change is
+# negligible (ie. .22%), so whole #s seems preferable here
+
+agg_total_pop <- left_join(by = "geography", total_pop_2017, total_pop_2016,
+                           suffix = c(".17", ".16")) %>% 
+  mutate(sum = totalpop.17 + totalpop.16) %>% 
+  mutate(change = ((totalpop.17 - totalpop.16)))
