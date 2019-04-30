f43e8cde6b9f8d1bf247aa7b5461021704d9cd83
Beau Meche <beau_meche@college.harvard.edu>
2019-04-29 21:55:30 -0400

    more progress

--- US_demographic_vis/app.R
+++ US_demographic_vis/app.R
@@ -23,7 +23,7 @@ ui <- fluidPage(
   mainPanel( 
       tabPanel("Population Flux",
                selectInput("info_1", "What Do You Find Interesting?",
-               c("Total Pop. in 2017" = "totalpop.17",
+               choices = c("Total Pop. in 2017" = "totalpop.17",
                  "Total Pop. in 2016" = "totalpop.16",
                  "What Changed?" = "change"),
                leafletOutput("all_states")
@@ -34,16 +34,16 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-  
+  ## need to insert an if-else statement thingy here
   output$all_states <- renderLeaflet({
     
     states <- states()
     all_us <- geo_join(states, agg_total_pop_2, "NAME", "geography")
     
-    bins <- c(-45000, -15000, 0, 15000, 30000,
-              45000, 100000, 200000, Inf)
+    # bins <- c(-45000, -15000, 0, 15000, 30000,
+    #           45000, 100000, 200000, Inf)
     
-    pal <- colorBin("YlOrRd", domain = all_us$get(info_1), bins = bins)
+    pal <- colorBin("YlOrRd", domain = all_us$info_1)
     
     # labels <- sprintf(
     #   "<strong>%s</strong><br/>%g people",
@@ -56,7 +56,7 @@ server <- function(input, output) {
         id = "mapbox.light",
         accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
       addPolygons(
-        fillColor = ~pal(all_us$get(info_1)),
+        fillColor = ~pal(info_1),
         weight = 2,
         opacity = 1,
         color = "white",
@@ -73,7 +73,7 @@ server <- function(input, output) {
           style = list("font-weight" = "normal", padding = "3px 8px"),
           textsize = "15px",
           direction = "auto")) %>%
-      addLegend(pal = pal, values = ~all_us$get(info_1), opacity = 0.7, title = NULL,
+      addLegend(pal = pal, values = ~all_us$info_1, opacity = 0.7, title = NULL,
                 position = "bottomright")
   })
     
